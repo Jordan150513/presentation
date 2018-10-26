694792399e12d4ae276813d91ea7ba3b9dd364ae
qiaodandan <qiaodandan@autohome.com.cn>
2018-10-26 16:43:42 +0800

    hystrix的监控

--- demo1/src/main/java/com/qiao/demo1/Demo1Application.java
+++ demo1/src/main/java/com/qiao/demo1/Demo1Application.java
@@ -1,11 +1,14 @@
 package com.qiao.demo1;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @EnableHystrix
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class Demo1Application {
 
@@ -21,6 +25,21 @@ public class Demo1Application {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public ServletRegistrationBean getServlet(){
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+
+        registrationBean.setLoadOnStartup(1);
+
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+
+        registrationBean.setName("HystrixMetricsStreamServlet");
+
+        return registrationBean;
     }
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
--- demo1/src/main/java/com/qiao/demo1/feign/ProviderFeign.java
+++ demo1/src/main/java/com/qiao/demo1/feign/ProviderFeign.java
@@ -4,13 +4,11 @@ import com.qiao.demo1.common.BasicOut;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//@FeignClient(name = "microservice-account-wallet")
-@FeignClient(name = "microservice-providerdemo")
+
+@FeignClient(name = "providerdemo")
 public interface ProviderFeign {
 
-//    @RequestMapping(value = "/wallet/ChargeController/chargeForUser",method = RequestMethod.GET)
-//    public String chargeForUser();
-    @RequestMapping(value = "/provider/echo/",method = RequestMethod.GET)
+    @RequestMapping(value = "/test/echo/",method = RequestMethod.GET)
     public String echo();
 }
 
